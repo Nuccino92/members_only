7e8ea761ac1f56a0e5183ec557e1604ce98cb447
Nuccino92 <anthnucci@hotmail.com>
2022-01-06 21:53:15 -0500

    app use url encoded/static files

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => res.render("index"));
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
