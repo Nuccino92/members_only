a47eb015d5b01c02b124de88a7f6b4fa63725e05
Nuccino92 <anthnucci@hotmail.com>
2022-01-06 21:52:50 -0500

    connect to mongoose, setup up server

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
+const dbuRI = process.env.MONGOOSE_URI;
+
+mongoose.connect(dbuRI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
 const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
