460cec3d7e1db6ca5906e23bbb53c7afdcf88a72
Nuccino92 <anthnucci@hotmail.com>
2022-01-08 22:50:03 -0500

    route setup

---
+++ routes/sign-upRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const signUpController = require("../controllers/sign-upController");
+
+router.get("/", signUpController.signUp_Index);
+router.post("/", signUpController.signUp_post);
+
+module.exports = router;
