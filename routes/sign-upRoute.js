b498606a0d56aa5f772fb4f6a29cc4c63d8f3ee8
Nuccino92 <anthnucci@hotmail.com>
2022-01-12 20:05:20 -0500

    refactor

--- routes/sign-upRoute.js
+++ routes/sign-upRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { body } = require("express-validator");
+const { validateUser } = require("../validation/sign-upValidator");
+
 const router = express.Router();
 
 const signUpController = require("../controllers/sign-upController");
 
 router.get("/", signUpController.signUp_Index);
-router.post("/", signUpController.signUp_post);
+router.route("/").post(validateUser, signUpController.signUp_post);
 
 module.exports = router;
