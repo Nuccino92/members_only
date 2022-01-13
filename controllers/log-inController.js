89bda5153642f2b5a179f175cec191712686860c
Nuccino92 <anthnucci@hotmail.com>
2022-01-12 19:55:47 -0500

    added flash messaging if login failed

--- controllers/log-inController.js
+++ controllers/log-inController.js
@@ -1,13 +1,25 @@
 const passport = require("passport");
 
 const logIn_Index = (req, res) => {
-  res.render("log-in");
+  const flashMessages = res.locals.getMessages();
+
+  if (flashMessages.error) {
+    res.render("log-in", {
+      showErrors: true,
+      errors: flashMessages.error,
+    });
+  } else {
+    res.render("log-in", { showErrors: false });
+  }
+
+  res.render("log-in", { showErrors: false });
 };
 
 const logIn_Post = (req, res, next) => {
   passport.authenticate("local", {
     successRedirect: "/",
-    failureRedirect: "/",
+    failureRedirect: "/log-in",
+    failureFlash: true,
   })(req, res, next);
 };
 
