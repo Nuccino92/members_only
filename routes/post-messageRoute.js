5ec012e6dde10d3d93ba62fab9ade1cc1a56117e
Nuccino92 <anthnucci@hotmail.com>
2022-01-14 23:01:42 -0500

    added controller

--- routes/post-messageRoute.js
+++ routes/post-messageRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { postMessage_Index } = require("../controllers/post-messageController");
+const {
+  postMessage_Index,
+  postMessage_Post,
+} = require("../controllers/post-messageController");
 
 const router = express.Router();
 
 router.get("/", postMessage_Index);
+router.post("/", postMessage_Post);
 
 module.exports = router;
