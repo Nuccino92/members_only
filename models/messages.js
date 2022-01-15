e920f3130fd2f5b1d9a1e1227688c9242b48fdbc
Nuccino92 <anthnucci@hotmail.com>
2022-01-14 23:00:50 -0500

    refactor

--- models/messages.js
+++ models/messages.js
@@ -4,20 +4,23 @@ const Schema = mongoose.Schema;
 
 const Message = mongoose.model(
   "Message",
-  new Schema({
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+  new Schema(
+    {
+      user: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      message: {
+        type: String,
+        required: true,
+      },
     },
-    title: {
-      type: String,
-      required: true,
-    },
-    message: {
-      type: String,
-      required: true,
-    },
-  }),
-  { timestamps: true }
+    { timestamps: true }
+  )
 );
+
+module.exports = Message;
