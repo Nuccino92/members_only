2a84bb5bbffaee924315b448c32324a87454df69
Nuccino92 <anthnucci@hotmail.com>
2022-01-14 23:00:42 -0500

    messages post controller

--- controllers/post-messageController.js
+++ controllers/post-messageController.js
@@ -6,10 +6,26 @@ const postMessage_Index = (req, res) => {
     : res.render("post-message", { user: req.user });
 };
 
-// const postMessage_Post = (req, res) => {
+const postMessage_Post = (req, res) => {
+  const { title, message } = req.body;
 
-// }
+  console.log();
+
+  const post = new Message({
+    user: req.user.username,
+    title: title,
+    message: message,
+  });
+
+  post.save((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+};
 
 module.exports = {
   postMessage_Index,
+  postMessage_Post,
 };
